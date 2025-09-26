70bd0adf3f7383aafccced2e30ff87b4fafcc7f4
Basil <48308815+Morzhling@users.noreply.github.com>
2025-09-26 14:20:35 +0300

    while added

--- bridges.py
+++ bridges.py
@@ -1,4 +1,27 @@
-n = int(input("количество мостов: "))
+n = int(input("Количество мостов: "))
+parts = input('Высоты мостов через пробел: ').split()
+heights = []
+
+for i in range(n):
+    heights.append(int(parts[i]))
+
+bus_height = 437
+crash = False
+i = 0
+
+while i < n and not crash:
+    if heights[i] <= bus_height:
+        print("Crash", i + 1)
+        crash = True
+    i += 1
+
+if not crash:
+    print("No crash")
+
+
+
+
+'''n = int(input("Количество мостов: "))
 
 parts = input('Высоты мостов через пробел: ').split()
 heights = []
@@ -16,4 +39,4 @@ for i in range(n):
         break
 
 if not crash:
-    print("No crash")
+    print("No crash")'''
